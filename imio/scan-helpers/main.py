d530010baf999e6715b1a1bde4195ebe03963c3f
Stephan Geulette <s.geulette@imio.be>
2024-07-15 14:46:46 +0200

    Added requirements, gitignore

---
+++ imio/scan-helpers/main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of the imio.scan-helpers distribution (https://github.com/IMIO/imio.scan-helpers).
+# Copyright (c) 2023 IMIO
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+import sys
+
+
+def main():
+    """ call this as a lib
+    """
+    print("Hello from imio.scan-helpers in main.py")
+
+
+if __name__ == "__main__":
+    """ command line call
+    """
+    main()
