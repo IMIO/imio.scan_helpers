17df8987cd5425d075f09d35d8771236ebb49d4f
Stephan Geulette <s.geulette@imio.be>
2024-08-19 15:33:14 +0200

    More tests

--- imio/scan_helpers/tests.py
+++ imio/scan_helpers/tests.py
@@ -3,8 +3,13 @@ from utils import copy_sub_files
 from utils import download_update
 from utils import get_dated_backup_dir
 from utils import get_download_dir_path
+from utils import get_last_dated_backup_dir
+from utils import get_latest_release_version
 from utils import get_main_backup_dir
+from utils import get_parameter
 from utils import get_scan_profiles_dir
+from utils import read_dir
+from utils import set_parameter
 
 import os
 import shutil
@@ -51,11 +56,55 @@ class TestUtils(unittest.TestCase):
     def test_get_download_dir_path(self):
         self.assertTrue(get_download_dir_path().endswith(p("imio.scan_helpers/_downloads")))
 
+    def test_get_last_dated_backup_dir(self):
+        self.assertIsNone(get_last_dated_backup_dir(p("test_env/kofax_backup")))
+        os.makedirs(p("test_env/kofax_backup/2000-01-01"))
+        os.makedirs(p("test_env/kofax_backup/2000-01-02"))
+        self.assertEqual(get_last_dated_backup_dir(p("test_env/kofax_backup")), p("test_env/kofax_backup/2000-01-02"))
+        shutil.rmtree(p("test_env/kofax_backup/2000-01-01"))
+        shutil.rmtree(p("test_env/kofax_backup/2000-01-02"))
+
+    def test_get_latest_release_version(self):
+        tag_name, download_url = get_latest_release_version({}, release="0.2.5")
+        self.assertEqual(tag_name, "0.2.5")
+        self.assertTrue(download_url.endswith("imio-scan-helpers-0.2.5.zip"))
+        tag_name, download_url = get_latest_release_version({})
+        self.assertTrue(tag_name >= "0.3.0")
+
     def test_get_main_backup_dir(self):
         self.assertEqual(get_main_backup_dir(create=False), p("test_env/kofax_backup"))
 
-    def test_scan_profiles_dir(self):
+    def test_get_set_parameter(self):
+        self.assertEqual(get_parameter(p("test_env/configuration.json")), {})
+        self.assertIsNone(get_parameter(p("test_env/configuration.json"), "a_key"))
+        dic = set_parameter(p("test_env/configuration.json"), "a_key", "a_value")
+        self.assertTrue(os.path.exists(p("test_env/configuration.json")))
+        self.assertEqual(get_parameter(p("test_env/configuration.json"), "a_key"), "a_value")
+        self.assertDictEqual(dic, {"a_key": "a_value"})
+        os.remove(p("test_env/configuration.json"))
+
+    def test_get_scan_profiles_dir(self):
         self.assertEqual(get_scan_profiles_dir(), p("test_env/ProgramData/Kofax/Kofax Express 3.2/Jobs"))
+
+    def test_read_dir(self):
+        self.assertEqual(
+            read_dir(p("test_env/ProgramData/Kofax/Kofax Express 3.2/Jobs")), ["IMIO ENTRANT", "IMIO SORTANT"]
+        )
+        self.assertEqual(
+            read_dir(p("test_env/ProgramData/Kofax/Kofax Express 3.2/Jobs"), with_path=True),
+            [
+                p("test_env/ProgramData/Kofax/Kofax Express 3.2/Jobs/IMIO ENTRANT"),
+                p("test_env/ProgramData/Kofax/Kofax Express 3.2/Jobs/IMIO SORTANT"),
+            ],
+        )
+        self.assertEqual(
+            read_dir(p("test_env/ProgramData/Kofax/Kofax Express 3.2/Jobs"), only_folders=True),
+            ["IMIO ENTRANT", "IMIO SORTANT"],
+        )
+        self.assertEqual(read_dir(p("test_env/ProgramData/Kofax/Kofax Express 3.2/Jobs"), only_files=True), [])
+        self.assertEqual(
+            read_dir(p("test_env/ProgramData/Kofax/Kofax Express 3.2/Jobs"), to_skip=["IMIO ENTRANT"]), ["IMIO SORTANT"]
+        )
 
 
 if __name__ == "__main__":
