cd2ef00f071fbf2f6b4861610a260f75162b5b0c
Stephan Geulette <s.geulette@imio.be>
2024-08-28 09:09:24 +0200

    Corrected typo

--- imio/scan_helpers/main.py
+++ imio/scan_helpers/main.py
@@ -166,7 +166,7 @@ if ns.auto_started and not get_parameter(params_file, "various"):
     set_parameter(params_file, "various", "auto_started")
 if ns.test_message:
     send_log_message(
-        "Test message\nInstalled in '{bundle_dir}'.\nVersion is {parameters['version']}",
+        f"Test message\nInstalled in '{bundle_dir}'.\nVersion is {parameters['version']}",
         parameters,
         log_method=log.info,
     )
